49e5bf982681d56b376e3707ce3cac398f870462
adeliaaaa <monicadelia88@gmail.com>
2022-12-01 13:52:50 +0700

    feat: remove three js

--- src/js/script.js
+++
@@ -1,7 +0,0 @@
-import * as THREE from 'three';
-
-const renderer = new THREE.WebGLRenderer();
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-document.body.appendChild(renderer.domElement);
