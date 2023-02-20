ae7dde20e6254df9b722929f8d7bc5fb17b0a66a
adeliaaaa <monicadelia88@gmail.com>
2023-02-20 13:43:38 +0700

    add three js rendering to php

--- pages/details.php
+++ pages/details.php
@@ -1,7 +1,19 @@
-<div>
-    detail product here
+<!-- <div class="container"> -->
+<?php
+    
+?>
+    <canvas id="myCanvas">    </canvas>
+    <input id="ph1" name="photo" type="file" onchange="getFileData(this);" label="fileee"/>
+    
 
 
 
-
-</div>
+<!-- </div> -->
+<script type="module" src="script.js"> </script>
+<script>
+    //   function getFileData(myFile){
+    //     var file = myFile.files[0];  
+    //     var filename = file.name;
+    //     console.log(filename)
+    //   }
+</script>
