ff3ce3ffd6d96e5b8c75136786e13f3a31445854
adeliaaaa <monicadelia88@gmail.com>
2023-05-23 19:45:07 +0700

    fix: image uploader button

--- pages/details.php
+++ pages/details.php
@@ -31,43 +31,44 @@
 
             <div class="container-top-right">
                 <button class="back-container" onClick="location.href='home'">
-                        <img src="./assets/Back-Button.png">
+                    <img src="./assets/Back-Button.png">
                 </button>
             </div>
 
             <div class="item-name-container">
                 <p id="myText" class="text-file-name"><?php echo $global_variable; ?></p>
             </div>
+            <form action="function.php" method="POST" enctype="multipart/form-data">
+                <div class="container-bottom-right">
+                    <input type="file" id="fileUpload" type="file" name="fileUpload" onChange="onFileChange()">
+                    <label for="fileUpload" class="upload-container">
+                        <div class="menu-container">
+                            <img src="./assets/Upload-File-Button.png">
+                        </div>
+                    </label>
+                </div>
 
-            
-                <form action="function.php" method="POST" enctype="multipart/form-data">
-                    <div class="container-bottom-right">
-                            <label for="fileUpload" class="upload-container">
-                                <img src="./assets/Upload-File-Button.png">
-                                <input id="fileUpload" type="file" name="fileUpload" onChange="onFileChange()" style="display:none">
-                            </label>
-                    </div>
-
-                    <div class="pop-up-container" id="submit-file-container">
-                        <div class="pop-up-box">
-                            <div class="pop-up-title">Upload Model</div>
-                            <div class="pop-up-text">Do you want to upload this model?</div>
-                            <div class="pop-up-button-container">
-                                <input type="submit" value="Upload" class="pop-up-button">
-                                <button type="button" class="pop-up-button" onclick="cancelSubmit()"> cancel </button>
-                            </div>
+                <div class="pop-up-container" id="submit-file-container">
+                    <div class="pop-up-box">
+                        <div class="pop-up-title">Upload Model</div>
+                        <div class="pop-up-text">Do you want to upload this model?</div>
+                        <div class="pop-up-text" id="pop-up-text-fileName">File name</div>
+                        <div class="pop-up-button-container">
+                            <input type="submit" value="Upload" class="pop-up-button">
+                            <button type="button" class="pop-up-button" onclick="cancelSubmit()"> cancel </button>
                         </div>
                     </div>
-                    
-                </form>
-            
+                </div>
+            </form>
         </div>
 
         <script type="module" src="script.js"> </script>
         <script> 
             function onFileChange(){
-                console.log("kesini");
                 document.getElementById("submit-file-container").style.display = "block";
+                var name = document.getElementById('fileUpload'); 
+                var nameContainer = document.getElementById('pop-up-text-fileName'); 
+                nameContainer.innerHTML = name.files.item(0).name
             }
 
             function cancelSubmit(){
