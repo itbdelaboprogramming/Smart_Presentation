4e36029268c1035c0a7223f33179ef7fba182214
adeliaaaa <monicadelia88@gmail.com>
2023-06-02 13:13:15 +0700

    feat: update data in database

--- database/config.php
+++ database/config.php
@@ -43,9 +43,9 @@
         }
     }
 
-    function getModelName($modelname){
+    function getModelName($category){
         $conn = connect();
-        $sql = "SELECT DISTINCT model.id, model.model_name, model.image_preview, model_detail.category, model_detail.description FROM model INNER JOIN model_detail ON model.model_name = model_detail.model_name WHERE category = '$modelname' ORDER BY model_name;";
+        $sql = "SELECT DISTINCT model.id, model.model_name, model.image_preview, model_detail.category, model_detail.description FROM model INNER JOIN model_detail ON model.model_name = model_detail.model_name WHERE category = '$category' ORDER BY model_name;";
         $result = mysqli_query($conn, $sql);
         $resultCheck = mysqli_num_rows($result);
     
@@ -53,6 +53,21 @@
             while($row = $result->fetch_assoc()){
                 $product2[] = $row;
             }
+            return $product2;
+        }
+    }
+
+    function getDescription($modelname){
+        $conn = connect();
+        $sql = "SELECT model.model_number, model_detail.model_name, model_detail.description, model_detail.specification, model_detail.link_to_web FROM model_detail INNER JOIN model ON model_detail.model_name = model.model_name WHERE model_detail.model_name = '$modelname';";
+        $result = mysqli_query($conn, $sql);
+        $resultCheck = mysqli_num_rows($result);
+    
+        if($resultCheck > 0){
+            while($row = $result->fetch_assoc()){
+                $product2[] = $row;
+            }
+
             return $product2;
         }
     }
