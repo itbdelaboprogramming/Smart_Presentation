20f207d6c803d03eb6993f3f466e7b432472bc35
adeliaaaa <monicadelia88@gmail.com>
2023-06-29 11:42:51 +0700

    style: formatting code structure database.js

--- js/databases.js
+++ js/databases.js
@@ -2,19 +2,25 @@ import { scene, camera } from "../script.js";
 import * as THREE from "three";
 import { GLTFLoader } from "https://unpkg.com/three@0.139.2/examples/jsm/loaders/GLTFLoader.js";
 
-// Const, Var, Let
-// ------------ dark/light mode toggle ------------
+// ---------------------------------------------------------------------------------------
+// -------------------------------- Const, Var, Let --------------------------------------
+// ---------------------------------------------------------------------------------------
+
+// ------------------------------ dark/light mode toggle ---------------------------------
 const toggle = document.querySelector(".toggle");
 let getMode = localStorage.getItem("mode");
 
-// ------------ filter (ascending/descending) ------------
+// --------------------------- filter (ascending/descending) -----------------------------
 const filter_sort_box = document.querySelector(".filter-sort-box");
 const filter_asc = document.getElementById("filter-asc");
 const filter_desc = document.getElementById("filter-desc");
+let order_type = "ASC";
 
-// ------------ filter (sort by) ------------
+// --------------------------------- filter (sort by) ------------------------------------
+let filter_drop_down = document.querySelector(".filter-drop-down");
 const filter_text = document.querySelector(".filter-text");
 
+let selected_sort_by = "model_name";
 const divMNa = document.getElementById("div_sort_model_name");
 const radMNa = document.getElementById("sort_model_name");
 const divMNu = document.getElementById("div_sort_model_number");
@@ -22,7 +28,7 @@ const radMNu = document.getElementById("sort_model_number");
 const divDM = document.getElementById("div_sort_date_modified");
 const radDM = document.getElementById("sort_date_modified");
 
-// ------------ pagination drop down ------------
+// --------------------------------- pagination drop down --------------------------------
 const optionMenu_pagination = document.querySelector(".pagination-select-menu");
 const selectBtn_pagination = optionMenu_pagination.querySelector(
 	".pagination-select-menu-button"
@@ -33,7 +39,47 @@ const sBtn_text_pagination = optionMenu_pagination.querySelector(
 	".pagination-select-menu-text"
 );
 
-// dark/light mode toggle
+// ------------------------------- pagination previous/next ------------------------------
+const total_data = document.querySelector(".total-data");
+const total_page = document.getElementById("total-page");
+const total_data_first = document.querySelector(".total-data-first");
+const total_data_last = document.querySelector(".total-data-last");
+const current_page = document.getElementById("current-page");
+const pagination_first_page_button = document.getElementById(
+	"pagination-first-page-button"
+);
+const pagination_previous_page_button = document.getElementById(
+	"pagination-previous-page-button"
+);
+const pagination_next_page_button = document.getElementById(
+	"pagination-next-page-button"
+);
+const pagination_last_page_button = document.getElementById(
+	"pagination-last-page-button"
+);
+
+// ---------------------------- resize canvas width responsive ---------------------------
+let right_content = document.querySelector(".right-content");
+let width = right_content.offsetWidth * 0.9;
+
+// ----------------------------------- database data -------------------------------------
+const database_table = document.getElementById("database-data");
+let selected_database_data_selected;
+
+let loader = new GLTFLoader();
+loader.name = "loader";
+
+// -------------------------------- 3d category dropdown ---------------------------------
+const optionMenu = document.querySelector(".select-menu");
+const selectBtn = optionMenu.querySelector(".select-menu-button");
+const options = optionMenu.querySelectorAll(".option");
+const sBtn_text = optionMenu.querySelector(".select-menu-text");
+
+// ---------------------------------------------------------------------------------------
+// ---------------------------------- PROGRAM CODE ---------------------------------------
+// ---------------------------------------------------------------------------------------
+
+// ------------------------------ dark/light mode toggle ---------------------------------
 if (getMode && getMode === "dark-theme") {
 	document.body.classList.add("dark-theme");
 	toggle.classList.add("active");
@@ -41,7 +87,6 @@ if (getMode && getMode === "dark-theme") {
 	scene.background = new THREE.Color(0x98a2b3);
 
 	scene.remove(scene.getObjectByName("grid"));
-
 	const grid = new THREE.GridHelper(20, 20, 0x475b74, 0x475b74);
 	grid.name = "grid";
 	scene.add(grid);
@@ -58,7 +103,6 @@ toggle.addEventListener("click", () => {
 		scene.background = new THREE.Color(0x98a2b3);
 
 		scene.remove(scene.getObjectByName("grid"));
-
 		const grid = new THREE.GridHelper(20, 20, 0x475b74, 0x475b74);
 		grid.name = "grid";
 		scene.add(grid);
@@ -76,8 +120,7 @@ toggle.addEventListener("click", () => {
 	}
 });
 
-let order_type = "ASC";
-// filter (ascending/descending)
+// --------------------------- filter (ascending/descending) -----------------------------
 filter_sort_box.addEventListener("click", () => {
 	if (filter_asc.style.display != "none") {
 		filter_asc.style.display = "none";
@@ -102,10 +145,8 @@ filter_sort_box.addEventListener("click", () => {
 	}
 });
 
-// filter (sort by)
-let filter_drop_down = document.querySelector(".filter-drop-down");
+// --------------------------------- filter (sort by) ------------------------------------
 filter_text.addEventListener("click", () => {
-	// do something
 	if (filter_drop_down.style.display == "none") {
 		filter_drop_down.style.display = "flex";
 	} else {
@@ -121,7 +162,6 @@ window.addEventListener("click", function (e) {
 	}
 });
 
-let selected_sort_by = "model_name";
 divMNa.addEventListener("click", () => {
 	radMNa.checked = true;
 	selected_sort_by = "model_name";
@@ -158,53 +198,13 @@ divDM.addEventListener("click", () => {
 	filter_drop_down.style.display = "none";
 });
 
-// pagination previous/next
-const total_data = document.querySelector(".total-data");
-const total_page = document.getElementById("total-page");
-const total_data_first = document.querySelector(".total-data-first");
-const total_data_last = document.querySelector(".total-data-last");
-const current_page = document.getElementById("current-page");
-const pagination_first_page_button = document.getElementById(
-	"pagination-first-page-button"
-);
-const pagination_previous_page_button = document.getElementById(
-	"pagination-previous-page-button"
-);
-const pagination_next_page_button = document.getElementById(
-	"pagination-next-page-button"
-);
-const pagination_last_page_button = document.getElementById(
-	"pagination-last-page-button"
-);
-
+// ------------------------------- pagination previous/next ------------------------------
 updateTotalPage();
 
-function updateTotalPage() {
-	total_page.innerText = Math.ceil(
-		total_data.innerText / sBtn_text_pagination.innerText
-	);
-
-	if (total_page.innerText == 1) {
-		pagination_next_page_button.classList.add("disabled");
-		pagination_last_page_button.classList.add("disabled");
-	} else {
-		pagination_next_page_button.classList.remove("disabled");
-		pagination_last_page_button.classList.remove("disabled");
-	}
-}
-
-// const database_data = document.querySelectorAll("tr");
-const database_table = document.getElementById("database-data");
-let selected_database_data_selected;
+// ----------------------------------- database data -------------------------------------
 updateDatabaseDataSelect(database_table);
 
-// ------------ 3d category dropdown ------------
-const optionMenu = document.querySelector(".select-menu");
-const selectBtn = optionMenu.querySelector(".select-menu-button");
-const options = optionMenu.querySelectorAll(".option");
-const sBtn_text = optionMenu.querySelector(".select-menu-text");
-
-// for 3d category dropdown
+// -------------------------------- 3d category dropdown ---------------------------------
 selectBtn.addEventListener("click", () => {
 	optionMenu.classList.toggle("active");
 });
@@ -231,7 +231,7 @@ window.addEventListener("click", function (e) {
 	}
 });
 
-// for pagination dropdown
+// --------------------------------- pagination drop down --------------------------------
 selectBtn_pagination.addEventListener("click", () => {
 	optionMenu_pagination.classList.toggle("active");
 });
@@ -264,25 +264,22 @@ window.addEventListener("click", function (e) {
 	}
 });
 
-// canvas
-let right_content = document.querySelector(".right-content");
-// let width = window.innerWidth * 0.4;
-let width = right_content.offsetWidth * 0.9;
+// ---------------------------- resize canvas width responsive ---------------------------
 resizeCanvas();
 
 window.addEventListener("resize", () => {
 	resizeCanvas();
 });
 
-// FUNCTION HELPER
+// ---------------------------------------------------------------------------------------
+// ---------------------------------- FUNCTION HELPER ------------------------------------
+// ---------------------------------------------------------------------------------------
+
+// ---------------------------- resize canvas width responsive ---------------------------
 function resizeCanvas() {
 	right_content = document.querySelector(".right-content");
 
 	width = right_content.offsetWidth * 0.9;
-
-	// if (width < 275) {
-	// 	width = 275;
-	// }
 
 	if (window.innerWidth <= 1140) {
 		width = window.innerWidth * 0.4;
@@ -293,12 +290,22 @@ function resizeCanvas() {
 	camera.updateProjectionMatrix();
 }
 
-function resetDatabaseDataSelect(database_data) {
-	database_data.forEach(function (data) {
-		data.classList.remove("active");
-	});
+// ------------------------------- pagination previous/next ------------------------------
+function updateTotalPage() {
+	total_page.innerText = Math.ceil(
+		total_data.innerText / sBtn_text_pagination.innerText
+	);
+
+	if (total_page.innerText == 1) {
+		pagination_next_page_button.classList.add("disabled");
+		pagination_last_page_button.classList.add("disabled");
+	} else {
+		pagination_next_page_button.classList.remove("disabled");
+		pagination_last_page_button.classList.remove("disabled");
+	}
 }
 
+// ----------------------------------- database data -------------------------------------
 function updateDatabaseData(amount, category, order_by, order_type) {
 	let http = new XMLHttpRequest();
 
@@ -382,6 +389,12 @@ function updateDatabaseData(amount, category, order_by, order_type) {
 	);
 }
 
+function resetDatabaseDataSelect(database_data) {
+	database_data.forEach(function (data) {
+		data.classList.remove("active");
+	});
+}
+
 function updateDatabaseDataSelect(database_table) {
 	const database_data = database_table.querySelectorAll("tr");
 
@@ -422,8 +435,6 @@ function loadFile3D(id) {
 	http.send("id=" + id);
 }
 
-let loader = new GLTFLoader();
-loader.name = "loader";
 function updateFile3D(file_name) {
 	try {
 		let file3D = scene.getObjectByName("file3D");
