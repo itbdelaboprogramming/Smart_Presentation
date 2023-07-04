a752fe4223fdc7c81a6813917c71847ceab10bc0
adeliaaaa <monicadelia88@gmail.com>
2023-07-04 10:48:51 +0700

    feat: 404 page design

--- pages/404.php
+++ pages/404.php
@@ -1,4 +1,25 @@
-<h1>404!!</h1>
-<h2>Page you are loooking for not available</h2>
-<a href="home">Go to Home</a>
 
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Smart Presentation</title>
+        <link rel="icon" type="image/x-icon" href="assets/logo.png">
+        <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./style/style.css" >
+        <link rel="stylesheet" href="./style/404.css" >
+
+    </head>
+    <body>
+        <div class="page-404">
+            <p class="title-404">404</p>
+            <h2>Oops, This Page Could Not Be Found</h2>
+            <p>Sorry but the page you are looking for does not exist, have been removed, name changed or is temporarily unavailable.</p>
+            <div class="button-container">
+                <a class="button-404" onclick="location.reload()">Reload Page</a>
+                <a class="button-404" href="home">Go to Home Page</a>
+            </div>
+        </div>
+
+    </body>
+</html>
